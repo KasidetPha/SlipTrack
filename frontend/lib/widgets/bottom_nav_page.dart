be66e06439462d2cfe86bb8e96faf5032337627d
KAS1DET <134573399+KasidetPha@users.noreply.github.com>
2025-08-19 12:31:58 +0700

    buttom navigationbar
    success

---
+++ frontend/lib/widgets/bottom_nav_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/add_page.dart';
+import 'package:frontend/pages/home_page.dart';
+import 'package:frontend/pages/profile_page.dart';
+// import 'package:lucide_icons/lucide_icons.dart';
+
+class BottomNavPage extends StatefulWidget {
+  const BottomNavPage({super.key});
+
+  @override
+  State<BottomNavPage> createState() => _BottomNavPageState();
+}
+
+class _BottomNavPageState extends State<BottomNavPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomePage(),
+    AddPage(),
+    ProfilePage()
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+            )
+          ]
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onTap,
+          elevation: 0,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: 'Add'),
+            BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Profile'),
+          ]
+        ),
+      ),
+    );
+  }
+}
